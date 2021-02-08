e9671a9282e632477da2b829201b8b0075a652d7
shenxinyu <1341156974@qq.com>
2021-02-08 16:37:47 +0800

    feat: add SquareLayoutManager SquareSnapHelper

---
+++ library/src/main/java/com/sinyu/android/library/square/SquareLayoutManager.kt
@@ -0,0 +1,428 @@
+package com.sinyu.android.library.square
+
+import android.animation.ValueAnimator
+import android.graphics.Point
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.*
+
+class SquareLayoutManager @JvmOverloads constructor(
+    val spanCount: Int = 3,
+    private val startPosition: Int = -1
+) : RecyclerView.LayoutManager() {
+
+    private var viewGap = 0f
+
+    val rowCount: Int by lazy {
+        ceil(itemCount / spanCount.toFloat()).toInt()
+    }
+
+    private var onceCompleteScrollLengthForVer = -1f
+    private var onceCompleteScrollLengthForHor = -1f
+
+    private var firstChildCompleteScrollLengthForVer = -1f
+    private var firstChildCompleteScrollLengthForHor = -1f
+
+    private var verScrollLock = false
+    private var horScrollLock = false
+
+    private var firstVisiblePos = 0
+
+    var childHeight = 0
+        private set
+
+    var childWidth = 0
+        private set
+
+    private var verticalOffset: Long = 0
+    private var horizontalOffset: Long = 0
+
+    private val verticalMaxOffset: Float
+        get() = if (childHeight == 0 || itemCount == 0) 0f else (childHeight + viewGap) * (rowCount - 1)
+
+    private val verticalMinOffset: Float
+        get() = if (childHeight == 0) 0f else (height - childHeight) / 2f
+
+    private val horizontalMaxOffset: Float
+        get() = if (childWidth == 0 || itemCount == 0) 0f else (childWidth + viewGap) * (spanCount - 1)
+
+    private val horizontalMinOffset: Float
+        get() = if (childWidth == 0) 0f else (width - childWidth) / 2f
+
+    var isAutoSelect = true
+    private var selectAnimator: ValueAnimator? = null
+
+    /**
+     * 初始化布局的时候，是否是定位到中心的 item
+     */
+    var isInitLayoutCenter = true
+    private var isFirstLayout = true
+
+    private var lastSelectedPosition = 0
+    private var onItemSelectedListener: (Int) -> Unit = {}
+
+    override fun generateDefaultLayoutParams(): RecyclerView.LayoutParams {
+        return RecyclerView.LayoutParams(
+            RecyclerView.LayoutParams.WRAP_CONTENT,
+            RecyclerView.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    override fun onLayoutChildren(recycler: RecyclerView.Recycler, state: RecyclerView.State) {
+        if (state.itemCount == 0) {
+            removeAndRecycleAllViews(recycler)
+            return
+        }
+        onceCompleteScrollLengthForVer = -1f
+        onceCompleteScrollLengthForHor = -1f
+
+        detachAndScrapAttachedViews(recycler)
+
+        onLayout(recycler, 0, 0)
+    }
+
+    private fun onLayout(
+        recycler: RecyclerView.Recycler,
+        dx: Int,
+        dy: Int
+    ): Point {
+        val pointResult = checkMoveLimit(dy, dx)
+
+        detachAndScrapAttachedViews(recycler)
+
+        var verStart: Float
+        var horStart: Float
+
+        val normalViewOffsetForVer: Float
+        val normalViewOffsetForHor: Float
+
+        var tempView: View? = null
+        var tempPosition = -1
+
+        val firstVisiblePosForVer: Int
+        val firstVisiblePosForHor: Int
+
+        if (onceCompleteScrollLengthForVer == -1f) {
+            tempPosition = firstVisiblePos
+            tempView = recycler.getViewForPosition(tempPosition)
+            measureChildWithMargins(tempView, 0, 0)
+            childHeight = getDecoratedMeasurementVertical(tempView)
+            childWidth = getDecoratedMeasurementHorizontal(tempView)
+        }
+
+        firstChildCompleteScrollLengthForVer = height / 2f + childHeight / 2f
+        firstChildCompleteScrollLengthForHor = width / 2f + childWidth / 2f
+
+        if (isInitLayoutCenter && isFirstLayout) {
+            isFirstLayout = false
+            val centerPos = rowCount / 2 * spanCount + spanCount / 2
+            val startPos = if (startPosition != -1) startPosition else centerPos
+            onItemSelectedListener(startPos)
+            lastSelectedPosition = startPos
+            val distanceForVer = calculateScrollToPositionVerOffset(startPos)
+            val distanceForHor = calculateScrollToPositionHorOffset(startPos)
+            verticalOffset += distanceForVer.toLong()
+            horizontalOffset += distanceForHor.toLong()
+        }
+
+        if (verticalOffset >= firstChildCompleteScrollLengthForVer) {
+            verStart = viewGap
+            onceCompleteScrollLengthForVer = childHeight + viewGap
+            firstVisiblePosForVer =
+                floor(abs(verticalOffset - firstChildCompleteScrollLengthForVer) / onceCompleteScrollLengthForVer.toDouble())
+                    .toInt() + 1
+            normalViewOffsetForVer =
+                abs(verticalOffset - firstChildCompleteScrollLengthForVer) % onceCompleteScrollLengthForVer
+        } else {
+            firstVisiblePosForVer = 0
+            verStart = verticalMinOffset
+            onceCompleteScrollLengthForVer = firstChildCompleteScrollLengthForVer
+            normalViewOffsetForVer = abs(verticalOffset) % onceCompleteScrollLengthForVer
+        }
+
+        if (horizontalOffset >= firstChildCompleteScrollLengthForHor) {
+            horStart = viewGap
+            onceCompleteScrollLengthForHor = childWidth + viewGap
+            firstVisiblePosForHor =
+                floor(abs(horizontalOffset - firstChildCompleteScrollLengthForHor) / onceCompleteScrollLengthForHor.toDouble())
+                    .toInt() + 1
+            normalViewOffsetForHor =
+                abs(horizontalOffset - firstChildCompleteScrollLengthForHor) % onceCompleteScrollLengthForHor
+        } else {
+            firstVisiblePosForHor = 0
+            horStart = horizontalMinOffset
+            onceCompleteScrollLengthForHor = firstChildCompleteScrollLengthForHor
+            normalViewOffsetForHor = abs(horizontalOffset) % onceCompleteScrollLengthForHor
+        }
+        firstVisiblePos = firstVisiblePosForVer * spanCount + firstVisiblePosForHor
+
+        verStart -= normalViewOffsetForVer
+        horStart -= normalViewOffsetForHor
+        val startLeft = horStart
+
+        var index = firstVisiblePos
+        var verCount = 1
+
+        while (index != -1) {
+            val item = if (index == tempPosition && tempView != null) {
+                tempView
+            } else {
+                recycler.getViewForPosition(index)
+            }
+
+            val focusPositionForVer =
+                (abs(verticalOffset) / (childHeight + viewGap)).toInt()
+            val focusPositionForHor =
+                (abs(horizontalOffset) / (childWidth + viewGap)).toInt()
+            val focusPosition = focusPositionForVer * spanCount + focusPositionForHor
+
+            if (index <= focusPosition) {
+                addView(item)
+            } else {
+                addView(item, 0)
+            }
+
+            measureChildWithMargins(item, 0, 0)
+
+            val left = horStart.toInt()
+            val top = verStart.toInt()
+            val right = left + getDecoratedMeasurementHorizontal(item)
+            val bottom = top + getDecoratedMeasurementVertical(item)
+
+            val minScale = 0.8f
+
+            val childCenterY = (top + bottom) / 2
+            val parentCenterY = height / 2
+            val fractionScaleY = abs(parentCenterY - childCenterY) / parentCenterY.toFloat()
+            val scaleX = 1.0f - (1.0f - minScale) * fractionScaleY
+
+            val childCenterX = (right + left) / 2
+            val parentCenterX = width / 2
+            val fractionScaleX = abs(parentCenterX - childCenterX) / parentCenterX.toFloat()
+            val scaleY = 1.0f - (1.0f - minScale) * fractionScaleX
+
+            item.scaleX = max(min(scaleX, scaleY), minScale)
+            item.scaleY = max(min(scaleX, scaleY), minScale)
+
+            layoutDecoratedWithMargins(item, left, top, right, bottom)
+
+            val verticalIndex = {
+                verStart += childHeight + viewGap
+                horStart = startLeft
+                if (verStart > height - paddingBottom) {
+                    index = -1
+                } else {
+                    index = firstVisiblePos + verCount * spanCount
+                    verCount++
+                }
+            }
+
+            if ((index + 1) % spanCount != 0) {
+                horStart += childWidth + viewGap
+                if (horStart > width - paddingRight) {
+                    verticalIndex()
+                } else {
+                    index++
+                }
+            } else {
+                verticalIndex()
+            }
+
+            if (index >= itemCount) {
+                index = -1
+            }
+        }
+        verScrollLock = false
+        horScrollLock = false
+        return pointResult
+    }
+
+    private fun checkMoveLimit(dy: Int, dx: Int): Point {
+        var dyResult = dy
+        var dxResult = dx
+
+        if (dyResult < 0) {
+            if (verticalOffset < 0) {
+                verticalOffset = 0.also { dyResult = it }.toLong()
+            }
+        }
+        if (dyResult > 0) {
+            if (verticalOffset >= verticalMaxOffset) {
+                verticalOffset = verticalMaxOffset.toLong()
+                dyResult = 0
+            }
+        }
+
+        if (dxResult < 0) {
+            if (horizontalOffset < 0) {
+                horizontalOffset = 0.also { dxResult = it }.toLong()
+            }
+        }
+        if (dxResult > 0) {
+            if (horizontalOffset >= horizontalMaxOffset) {
+                horizontalOffset = horizontalMaxOffset.toLong()
+                dxResult = 0
+            }
+        }
+        return Point(dxResult, dyResult)
+    }
+
+    override fun canScrollHorizontally() = horScrollLock.not()
+
+    override fun canScrollVertically() = verScrollLock.not()
+
+    override fun scrollHorizontallyBy(
+        dx: Int,
+        recycler: RecyclerView.Recycler,
+        state: RecyclerView.State
+    ): Int {
+        if (dx == 0 || childCount == 0) {
+            return 0
+        }
+        verScrollLock = true
+        if (abs(dx.toFloat()) < 0.00000001f) {
+            return 0
+        }
+        horizontalOffset += dx
+        return onLayout(recycler, dx, 0).x
+    }
+
+    override fun scrollVerticallyBy(
+        dy: Int,
+        recycler: RecyclerView.Recycler,
+        state: RecyclerView.State
+    ): Int {
+        if (dy == 0 || childCount == 0) {
+            return 0
+        }
+        horScrollLock = true
+        if (abs(dy.toFloat()) < 0.00000001f) {
+            return 0
+        }
+        verticalOffset += dy
+        return onLayout(recycler, 0, dy).y
+    }
+
+    override fun onScrollStateChanged(state: Int) {
+        super.onScrollStateChanged(state)
+        if (state == RecyclerView.SCROLL_STATE_DRAGGING) {
+            cancelAnimator()
+        }
+    }
+
+    override fun onAttachedToWindow(view: RecyclerView?) {
+        super.onAttachedToWindow(view)
+        if (isAutoSelect) {
+            SquareSnapHelper().attachToRecyclerView(view)
+        }
+    }
+
+    fun calculateDistanceToPositionForVer(targetPos: Int): Int {
+        return childHeight * (targetPos / spanCount) - verticalOffset.toInt()
+    }
+
+    fun calculateDistanceToPositionForHor(targetPos: Int): Int {
+        return childWidth * (targetPos % spanCount) - horizontalOffset.toInt()
+    }
+
+    fun smoothScrollToPosition(position: Int) {
+        if (position > -1 && position < itemCount) {
+            startValueAnimator(position)
+        }
+    }
+
+    fun smoothScrollToCenter() {
+        val centerPos = rowCount / 2 * spanCount + spanCount / 2
+        smoothScrollToPosition(centerPos)
+    }
+
+    override fun smoothScrollToPosition(
+        recyclerView: RecyclerView?,
+        state: RecyclerView.State?,
+        position: Int
+    ) {
+        smoothScrollToPosition(position)
+    }
+
+    fun setOnItemSelectedListener(listener: (Int) -> Unit) {
+        onItemSelectedListener = listener
+    }
+
+    private fun startValueAnimator(position: Int) {
+        cancelAnimator()
+        val distanceForVer = calculateScrollToPositionVerOffset(position)
+        val distanceForHor = calculateScrollToPositionHorOffset(position)
+
+        val minDuration: Long = 200
+        val maxDuration: Long = 400
+
+        val distanceFractionForVer: Float =
+            abs(distanceForVer) / (childHeight + viewGap)
+        val distanceFractionForHor: Float =
+            abs(distanceForHor) / (childWidth + viewGap)
+
+        val durationForVer = if (distanceForVer <= childHeight + viewGap) {
+            (minDuration + (maxDuration - minDuration) * distanceFractionForVer).toLong()
+        } else {
+            (maxDuration * distanceFractionForVer).toLong()
+        }
+
+        val durationForHor = if (distanceForHor <= childWidth + viewGap) {
+            (minDuration + (maxDuration - minDuration) * distanceFractionForHor).toLong()
+        } else {
+            (maxDuration * distanceFractionForHor).toLong()
+        }
+
+        val duration = max(durationForVer, durationForHor)
+
+        selectAnimator = ValueAnimator.ofFloat(0.0f, duration.toFloat()).apply {
+            this.duration = duration
+            interpolator = DecelerateInterpolator()
+
+            val startedOffsetForVer = verticalOffset.toFloat()
+            val startedOffsetForHor = horizontalOffset.toFloat()
+
+            addUpdateListener { animation ->
+                val value = animation.animatedValue as Float
+                verticalOffset =
+                    (startedOffsetForVer + value * (distanceForVer / duration.toFloat())).toLong()
+                horizontalOffset =
+                    (startedOffsetForHor + value * (distanceForHor / duration.toFloat())).toLong()
+
+                requestLayout()
+            }
+            doOnEnd {
+                if (lastSelectedPosition != position) {
+                    onItemSelectedListener(position)
+                    lastSelectedPosition = position
+                }
+            }
+            start()
+        }
+    }
+
+
+    fun cancelAnimator() {
+        selectAnimator?.takeIf { it.isStarted || it.isRunning }?.apply {
+            cancel()
+        }
+    }
+
+    private fun calculateScrollToPositionVerOffset(position: Int) =
+        position / spanCount * (childHeight + viewGap) - abs(verticalOffset)
+
+    private fun calculateScrollToPositionHorOffset(position: Int) =
+        position % spanCount * (childWidth + viewGap) - abs(horizontalOffset)
+
+    private fun getDecoratedMeasurementHorizontal(view: View): Int {
+        val params = view.layoutParams as RecyclerView.LayoutParams
+        return getDecoratedMeasuredWidth(view) + params.leftMargin + params.rightMargin
+    }
+
+    private fun getDecoratedMeasurementVertical(view: View): Int {
+        val params = view.layoutParams as RecyclerView.LayoutParams
+        return getDecoratedMeasuredHeight(view) + params.topMargin + params.bottomMargin
+    }
+}
---
+++ library/src/main/java/com/sinyu/android/library/square/SquareSnapHelper.kt
@@ -0,0 +1,296 @@
+package com.sinyu.android.library.square
+
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+import android.widget.Scroller
+import androidx.recyclerview.widget.OrientationHelper
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.roundToInt
+
+class SquareSnapHelper : RecyclerView.OnFlingListener() {
+
+    private var mVerticalHelper: OrientationHelper? = null
+    private var mHorizontalHelper: OrientationHelper? = null
+
+    private var mRecyclerView: RecyclerView? = null
+    private var mGravityScroller: Scroller? = null
+
+    private val mScrollListener: RecyclerView.OnScrollListener =
+        object : RecyclerView.OnScrollListener() {
+            var mScrolled = false
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == RecyclerView.SCROLL_STATE_IDLE && mScrolled) {
+                    mScrolled = false
+                    snapToTargetExistingView()
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (dx != 0 || dy != 0) {
+                    mScrolled = true
+                }
+            }
+        }
+
+    fun snapToTargetExistingView() {
+        val layoutManager = mRecyclerView?.layoutManager ?: return
+        val snapView: View = findSnapView(layoutManager) ?: return
+        val position = layoutManager.getPosition(snapView)
+        if (layoutManager !is SquareLayoutManager) {
+            return
+        }
+        layoutManager.smoothScrollToPosition(position)
+    }
+
+    fun attachToRecyclerView(recyclerView: RecyclerView?) {
+        mRecyclerView = recyclerView
+        recyclerView?.let {
+            mGravityScroller = Scroller(
+                it.context,
+                DecelerateInterpolator()
+            )
+            setupCallbacks()
+        }
+
+    }
+
+    private fun setupCallbacks() {
+        check(mRecyclerView?.onFlingListener == null) { "An instance of OnFlingListener already set." }
+        mRecyclerView?.addOnScrollListener(mScrollListener)
+        mRecyclerView?.onFlingListener = this
+    }
+
+    override fun onFling(velocityX: Int, velocityY: Int): Boolean {
+        val layoutManager = mRecyclerView?.layoutManager ?: return false
+        val minFlingVelocity = mRecyclerView?.minFlingVelocity ?: return false
+
+        return ((abs(velocityY) > minFlingVelocity || abs(velocityX) > minFlingVelocity) && snapFromFling(
+            layoutManager,
+            velocityX,
+            velocityY
+        ))
+    }
+
+    private fun snapFromFling(
+        layoutManager: RecyclerView.LayoutManager, velocityX: Int,
+        velocityY: Int
+    ): Boolean {
+        if (layoutManager !is SquareLayoutManager) {
+            return false
+        }
+        val targetPosition: Int =
+            findTargetSnapPosition(layoutManager, velocityX, velocityY)
+        if (targetPosition == RecyclerView.NO_POSITION) {
+            return false
+        }
+        layoutManager.smoothScrollToPosition(targetPosition)
+        return true
+    }
+
+    private fun findSnapView(layoutManager: RecyclerView.LayoutManager): View? {
+        return findCenterView(
+            layoutManager,
+            getVerticalHelper(layoutManager),
+            getHorizontalHelper(layoutManager)
+        )
+    }
+
+    private fun findCenterView(
+        layoutManager: RecyclerView.LayoutManager,
+        verHelper: OrientationHelper, horHelper: OrientationHelper
+    ): View? {
+        val childCount = layoutManager.childCount
+        if (childCount == 0) {
+            return null
+        }
+        var closestChild: View? = null
+        val verCenter = verHelper.startAfterPadding + verHelper.totalSpace / 2
+        val horCenter = horHelper.startAfterPadding + horHelper.totalSpace / 2
+
+        var verAbsClosest = Int.MAX_VALUE
+        var horAbsClosest = Int.MAX_VALUE
+
+        for (i in 0 until childCount) {
+            val child = layoutManager.getChildAt(i)
+
+            val childVerCenter = (verHelper.getDecoratedStart(child)
+                    + verHelper.getDecoratedMeasurement(child) / 2)
+            val verAbsDistance = abs(childVerCenter - verCenter)
+
+            val childHorCenter = (horHelper.getDecoratedStart(child)
+                    + horHelper.getDecoratedMeasurement(child) / 2)
+            val horAbsDistance = abs(childHorCenter - horCenter)
+
+            if (verAbsDistance < verAbsClosest || horAbsDistance < horAbsClosest) {
+                verAbsClosest = verAbsDistance
+                horAbsClosest = horAbsDistance
+                closestChild = child
+            }
+        }
+        return closestChild
+    }
+
+    private fun findTargetSnapPosition(
+        layoutManager: RecyclerView.LayoutManager, velocityX: Int,
+        velocityY: Int
+    ): Int {
+        if (layoutManager !is SquareLayoutManager) {
+            return RecyclerView.NO_POSITION
+        }
+
+        val itemCount = layoutManager.itemCount
+        if (itemCount == 0) {
+            return RecyclerView.NO_POSITION
+        }
+
+        val currentView = findSnapView(layoutManager) ?: return RecyclerView.NO_POSITION
+        val currentPosition = layoutManager.getPosition(currentView)
+        if (currentPosition == RecyclerView.NO_POSITION) {
+            return RecyclerView.NO_POSITION
+        }
+
+        var hDeltaJump =
+            if (layoutManager.canScrollHorizontally() && ((abs(velocityY) - abs(velocityX)) > 4000).not()) {
+                estimateNextPositionDiffForFling(
+                    layoutManager,
+                    getHorizontalHelper(layoutManager), velocityX, 0
+                )
+            } else {
+                0
+            }
+
+        var vDeltaJump =
+            if (layoutManager.canScrollVertically() && ((abs(velocityX) - abs(velocityY)) > 4000).not()) {
+                estimateNextPositionDiffForFling(
+                    layoutManager,
+                    getVerticalHelper(layoutManager), 0, velocityY
+                )
+            } else {
+                0
+            }
+
+        val spanCount = layoutManager.spanCount
+        val rowCount = layoutManager.rowCount
+
+        val currentHorPos = currentPosition % spanCount + 1
+        val currentVerPos = currentPosition / spanCount + 1
+
+        hDeltaJump = when {
+            currentHorPos + hDeltaJump >= spanCount -> {
+                abs(spanCount - currentHorPos)
+            }
+            currentHorPos + hDeltaJump <= 0 -> {
+                -(currentHorPos - 1)
+            }
+            else -> {
+                hDeltaJump
+            }
+        }
+
+        vDeltaJump = when {
+            currentVerPos + vDeltaJump >= rowCount -> {
+                abs(rowCount - currentVerPos)
+            }
+            currentVerPos + vDeltaJump <= 0 -> {
+                -(currentVerPos - 1)
+            }
+            else -> {
+                vDeltaJump
+            }
+        }
+
+        vDeltaJump = if (vDeltaJump > 0) min(3, vDeltaJump) else max(-3, vDeltaJump)
+        hDeltaJump = if (hDeltaJump > 0) min(3, hDeltaJump) else max(-3, hDeltaJump)
+
+        val deltaJump = hDeltaJump + vDeltaJump * spanCount
+
+        if (deltaJump == 0) {
+            return RecyclerView.NO_POSITION
+        }
+
+        var targetPos = currentPosition + deltaJump
+
+        if (targetPos < 0) {
+            targetPos = 0
+        }
+        if (targetPos >= itemCount) {
+            targetPos = itemCount - 1
+        }
+        return targetPos
+    }
+
+    private fun estimateNextPositionDiffForFling(
+        layoutManager: RecyclerView.LayoutManager,
+        helper: OrientationHelper, velocityX: Int, velocityY: Int
+    ): Int {
+        val distances = calculateScrollDistance(velocityX, velocityY) ?: return -1
+        val distancePerChild = computeDistancePerChild(layoutManager, helper)
+        if (distancePerChild <= 0) {
+            return 0
+        }
+        val distance =
+            if (abs(distances[0]) > abs(distances[1])) distances[0] else distances[1]
+        return (distance / distancePerChild).roundToInt()
+    }
+
+    private fun calculateScrollDistance(velocityX: Int, velocityY: Int): IntArray? {
+        val outDist = IntArray(2)
+        mGravityScroller?.fling(
+            0,
+            0,
+            velocityX,
+            velocityY,
+            Int.MIN_VALUE,
+            Int.MAX_VALUE,
+            Int.MIN_VALUE,
+            Int.MAX_VALUE
+        )
+        outDist[0] = mGravityScroller?.finalX ?: return null
+        outDist[1] = mGravityScroller?.finalY ?: return null
+        return outDist
+    }
+
+    private fun computeDistancePerChild(
+        layoutManager: RecyclerView.LayoutManager,
+        helper: OrientationHelper
+    ): Float {
+        val childCount = layoutManager.childCount
+        if (childCount == 0) {
+            return INVALID_DISTANCE
+        }
+        if (layoutManager !is SquareLayoutManager) {
+            return INVALID_DISTANCE
+        }
+
+        return if (helper == getVerticalHelper(layoutManager)) {
+            layoutManager.childHeight.toFloat()
+        } else {
+            layoutManager.childWidth.toFloat()
+        }
+    }
+
+    private fun getVerticalHelper(layoutManager: RecyclerView.LayoutManager): OrientationHelper {
+        if (mVerticalHelper == null) {
+            mVerticalHelper = OrientationHelper.createVerticalHelper(layoutManager)
+        }
+        return mVerticalHelper!!
+    }
+
+    private fun getHorizontalHelper(
+        layoutManager: RecyclerView.LayoutManager
+    ): OrientationHelper {
+        if (mHorizontalHelper == null) {
+            mHorizontalHelper = OrientationHelper.createHorizontalHelper(layoutManager)
+        }
+        return mHorizontalHelper!!
+    }
+
+    companion object {
+        private const val INVALID_DISTANCE = 1f
+    }
+
+}
